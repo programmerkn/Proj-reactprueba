6146cbd2e8b76ea2c84188f59921ff634b91b413
Marcos T <139595561+programmerkn@users.noreply.github.com>
2023-09-14 07:09:05 +0000

    Update MiBoton.jsx

--- src/MiBoton.jsx
+++ src/MiBoton.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 
 function MiBoton() {
   const [textoBoton, setTextoBoton] = useState("Haz clic aquí");
-
+const [contadorBoton,setContadorBoton ] = useState(0);
     const handleClick = () => {
-        setTextoBoton("Di clic");
+      setContadorBoton(contadorBoton +1);
+        setTextoBoton("Di clic"+contadorBoton );
           };
 
             return (
